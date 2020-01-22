d8f714a45761ab48d6432bec26aafbe57efc5ab1
Gerardo Sancho <gsancho@gsancho.com>
2020-01-22 20:06:38 +0100

    Init Problems Repository

--- app/src/main/java/com/team4/BoulderBuild/config/ApplicationDI.kt
+++ app/src/main/java/com/team4/BoulderBuild/config/ApplicationDI.kt
@@ -3,21 +3,29 @@ package com.team4.BoulderBuild.config
 import android.content.Context
 import com.team4.BoulderBuild.model.data.database.GymDatabase
 import com.team4.BoulderBuild.model.data.repository.GymsRepository
-import com.team4.BoulderBuild.model.data.source.GymsLocalDataSource
+import com.team4.BoulderBuild.model.data.repository.ProblemsRepository
 import com.team4.BoulderBuild.model.data.source.GymsRoomDataSource
+import com.team4.BoulderBuild.model.data.source.ProblemsRoomDataSource
 
-object ApplicationDI{
+object ApplicationDI {
     // TODO: use Koin in the future
 
-    private var context : Context? = null
-    private var gymsRepository : GymsRepository? = null
+    private var context: Context? = null
+    private lateinit var gymsRepository: GymsRepository
+    private lateinit var problemsRepository: ProblemsRepository
 
-    fun initDI(context: Context){
+    fun initDI(context: Context) {
         this.context = context
         gymsRepository = GymsRepository(GymsRoomDataSource(GymDatabase.buildDatabase(context)))
+        problemsRepository =
+            ProblemsRepository(ProblemsRoomDataSource(GymDatabase.buildDatabase(context)))
     }
 
-    fun getGymsRepository(): GymsRepository?{
+    fun getGymsRepository(): GymsRepository {
         return gymsRepository
+    }
+
+    fun getProblemsRepository(): ProblemsRepository {
+        return problemsRepository
     }
 }
